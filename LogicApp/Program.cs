2fa8f6472f6b5215936401bcb85b44fd37bf1833
Edon Bytyqi <edonbytyqi@outlook.com>
2025-05-28 14:33:21 +0200

    Add project files.

---
+++ LogicApp/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace LogicApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+
+            bool isRainy = true;
+            bool hasUmbrella = true;
+
+            // Logical Operators
+            // AND &&
+            // OR  ||
+            // NOT !
+
+            if (isRainy && hasUmbrella)
+            {
+                Console.WriteLine("I am protected against rain");
+            }
+
+            if (!isRainy || hasUmbrella)
+            {
+                Console.WriteLine("I am not getting wet");
+            }
+            Console.WriteLine("Ay OK!");
+            Console.ReadKey();
+        }
+    }
+}
